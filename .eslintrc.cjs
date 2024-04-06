6179414fe2fa1abb84af964e023ef805b96a3b44
Torie <vkim20@gmail.com>
2024-04-06 14:43:02 -0400

    chore: clean up App.tsx, eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,22 @@ module.exports = {
   root: true,
   env: { browser: true, node: true, es2020: true },
   extends: [
+    // `eslint-plugin-react`
     'eslint:recommended',
+    'plugin:react/recommended',
+    // To disable relevant rules when using new JSX transform from React 17
+    'plugin:react/jsx-runtime',
+    // `@typescript-eslint`
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/strict-type-checked',
+    // `eslint-plugin-react-hooks`
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    // `eslint-plugin-jsx-a11y`
     'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended', // Always last in array
+    // This sets up both `eslint-plugin-prettier` & `eslint-config-prettier` in one go
+    'plugin:prettier/recommended', // Always last in array to override other configs
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -17,12 +25,17 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname + '/src',
+    tsconfigRootDir: __dirname,
     ecmaFeatures: {
       jsx: true,
     },
   },
-  plugins: ['react-refresh', 'prettier', 'simple-import-sort'],
+  plugins: [
+    '@typescript-eslint',
+    'react-refresh',
+    'prettier',
+    'simple-import-sort',
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
@@ -30,11 +43,11 @@ module.exports = {
     ],
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
     'react/react-in-jsx-scope': 'off',
-    'jsx-a11y/accessible-emoji': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    'jsx-a11y/accessible-emoji': 'off',
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,16 @@
 import './App.css'
 
-import { useState } from 'react'
-
-import viteLogo from '/vite.svg'
-
-import reactLogo from './assets/react.svg'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h3>Boilerplate Code</h3>
+      <h1>React + TypeScript + Vite</h1>
+      <h2>ESLint + Prettier</h2>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
